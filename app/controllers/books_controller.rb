4411cd2997f064b6e171ed52af37e2e2876b65f9
EC2 Default User <ec2-user@ip-172-31-88-232.ec2.internal>
2022-01-26 05:35:40 +0000

    [Finish]応用課題２

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,15 +1,20 @@
 class BooksController < ApplicationController
 
+  before_action :baria_user, only: [:edit, :update]
+
   def show
     @book = Book.find(params[:id])
+    @newbook =Book.new
   end
 
   def index
+    @book = Book.new
     @books = Book.all
   end
 
   def create
     @book = Book.new(book_params)
+    @book.user_id = current_user.id
     if @book.save
       redirect_to book_path(@book), notice: "You have created book successfully."
     else
@@ -25,21 +30,27 @@ class BooksController < ApplicationController
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
-      redirect_to book_path(@book), notice: "You have updated book successfully."
+      redirect_to book_path(@book.id), notice: "You have updated book successfully."
     else
       render "edit"
     end
   end
 
-  def delete
+  def destroy
     @book = Book.find(params[:id])
-    @book.destoy
+    @book.destroy
     redirect_to books_path
   end
 
   private
 
   def book_params
-    params.require(:book).permit(:title)
+    params.require(:book).permit(:title, :body)
+  end
+
+  def baria_user
+    unless Book.find(params[:id]).user.id == current_user.id
+      redirect_to books_path
+    end
   end
 end
